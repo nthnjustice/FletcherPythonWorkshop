dbbdcadebe7e062f3f69c917a393559d05a73668
Nathan Justice <n.justice@outlook.com>
2015-11-11 20:02:17 -0500

    Added first session

---
+++ Round2/session1.py
@@ -0,0 +1,174 @@
+# Introductions and interests
+# My goals:
+	# To help teach a useful skill
+	# Remove the programming stigma (it's for everyone!)
+	# Teach the terminology so you can speak the lingo
+	# Teach problem solving skills
+	# Show the webpage
+# Introduction to programming in Python
+	# What is it? What can I do with it?
+	# Python 2.x vs. 3.x
+		# Significant, but not vast, differences
+		# Python 3 released in: December 3, 2008
+		# Using Python 3.x because:
+			# More long-term stability
+			# No more updates for 2.x
+		# Beware of version differences when looking at other people's code
+			# No backwards compatability
+		# http://sebastianraschka.com/Articles/2014_python_2_3_key_diff.html
+	# Learning a programming language is like learning a foreign spoken language
+		# Special vocabulary and syntax
+		# We all have the capacity to learn, practice makes perfect
+# Download IDLE
+	# IDLE basics
+		# Open the software
+		# What is the Interpreter and shell?
+# Arithmetic using the shell
+	# Addition
+	# Subtraction
+	# Multiplication
+	# Division
+		# Difference between Python 2.x and 3.x
+			# 3 / 2 = 1.5
+			# 3 // 2 = 1
+			# 3 / 2.0 = 1.5
+			# 3 // 2.0 = 1.0
+	# Exponents 
+# Making Python scripts
+	# How to open a new scripts
+	# How to progam a script
+		# WHITESPACE MATTERS
+	# Where to save the script
+	# How to run the script
+# Variables
+	# What is a variable?
+	# Why are they important?
+	# Variable types
+	# Using variables in the shell
+
+# Python programming...
+
+##############################################################################
+'''
+a = 123
+
+b = 122
+
+print(a)
+print(b)
+
+print(a + b)
+
+c = a + b
+
+print(c)
+
+'''
+##############################################################################
+'''
+x = 12345
+print(x)
+
+x = 6789
+print(x)
+
+'''
+##############################################################################
+'''
+
+a = 1.5
+
+print(a * 2)
+
+'''
+##############################################################################
+'''
+
+x = 3 ** 2
+
+print(x)
+
+'''
+##############################################################################
+'''
+
+x = 2 + 8 * 2
+
+print(x)
+
+x = (2 + 8) * 2
+
+print(x)
+
+'''
+##############################################################################
+'''
+
+If you type like this, your code won't work.
+
+It's important to annotate your code - for your future self and for others!
+
+Comments allow you to do this without breaking your script.
+
+# This is a comment. 
+# Comments are ignored by the Interpreter
+
+'''
+##############################################################################
+'''
+
+# comments can occupy a line of their own
+
+print(5) # comments can also be in-line
+
+
+# You can use block comments
+#for large amounts of text
+#that span multiple lines
+
+'''
+##############################################################################
+'''
+
+# Variable types
+
+a = 3
+b = 1.5
+c = "Hello"
+d = 'World'
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+e = 1
+print(a + e)
+
+f = 1.5
+print(b + f)
+
+print(a + b)
+
+print("BYE")
+
+g = c + "BYE"
+print(g)
+
+print(c + "BYE")
+
+h = c + d
+
+print(h)
+
+i = a + c
+print(i)
+
+j = b + d
+print(j)
+
+print(c + g + h + d) # Can you guess what this will print?
+
+'''
+##############################################################################
+
